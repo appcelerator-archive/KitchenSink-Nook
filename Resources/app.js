31cf43a53620a5d3b2fce6085b6143b6c7d49f1b
fusion94 <tguntharp@appcelerator.com>
2011-08-16 17:54:30 -0700

    adding new files to make it go

--- Resources/app.js
+++ Resources/app.js
@@ -4,130 +4,61 @@ Titanium.UI.setBackgroundColor('#000');
 // create tab group
 var tabGroup = Titanium.UI.createTabGroup();
 
-
-//
-// create base UI tab and root window
-//
 var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
+    title:'Base UI',
+    url:'main_windows/base_ui.js'
 });
 var tab1 = Titanium.UI.createTab({  
     icon:'KS_nav_views.png',
-    title:'Tab 1',
+    title:'Base UI',
     window:win1
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
 var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
+    title:'Controls',
+    url:'main_windows/controls.js'
 });
 var tab2 = Titanium.UI.createTab({  
     icon:'KS_nav_ui.png',
-    title:'Tab 2',
+    title:'Controls',
     window:win2
 });
 
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-//
-// create controls tab and root window
-//
 var win3 = Titanium.UI.createWindow({  
-    title:'Tab 3',
-    backgroundColor:'#fff'
+    title:'Miscellaneous',
+    url:'main_windows/phone.js'
 });
 var tab3 = Titanium.UI.createTab({  
     icon:'KS_nav_ui.png',
-    title:'Tab 3',
+    title:'Miscellaneous',
     window:win3
 });
 
-var label3 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 3',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win3.add(label3);
-
-//
-// create controls tab and root window
-//
 var win4 = Titanium.UI.createWindow({  
-    title:'Tab 4',
-    backgroundColor:'#fff'
+    title:'Platform',
+	url:'main_windows/platform.js'
 });
 var tab4 = Titanium.UI.createTab({  
     icon:'KS_nav_ui.png',
-    title:'Tab 4',
+    title:'Platform',
     window:win4
 });
 
-var label4 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 4',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win4.add(label4);
-
-//
-// create controls tab and root window
-//
 var win5 = Titanium.UI.createWindow({  
-    title:'Tab 5',
-    backgroundColor:'#fff'
+    title:'Mashups',
+    url:'main_windows/mashups.js'
 });
 var tab5 = Titanium.UI.createTab({  
     icon:'KS_nav_ui.png',
-    title:'Tab 5',
+    title:'Mashups',
     window:win5
 });
 
-var label5 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 5',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win5.add(label5);
-//
-//  add tabs
-//
 tabGroup.addTab(tab1);  
 tabGroup.addTab(tab2);  
 tabGroup.addTab(tab3);  
 tabGroup.addTab(tab4);  
 tabGroup.addTab(tab5);  
-
 
 // open tab group
 tabGroup.open();
---
+++ Resources/examples/movie_local.js
@@ -0,0 +1,91 @@
+var win = Titanium.UI.currentWindow;
+var android = (Titanium.Platform.name == 'android');
+
+var options = {
+	contentURL:'../movie.mp4',
+	backgroundColor:'#111',
+	scalingMode:Titanium.Media.VIDEO_SCALING_MODE_FILL
+};
+
+if (android) {
+	options.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_DEFAULT;
+} else {
+	if (parseFloat(Titanium.Platform.version) >= 3.2) {
+		options.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_NONE;
+	} else {
+		options.movieControlMode = Titanium.Media.VIDEO_CONTROL_NONE;
+	}
+}
+
+var activeMovie = Titanium.Media.createVideoPlayer(options);
+
+if (parseFloat(Titanium.Platform.version) >= 3.2)
+{
+	activeMovie.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_EMBEDDED;
+//	activeMovie.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_FULLSCREEN;
+//	activeMovie.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_NONE;
+	if (Titanium.Platform.osname == "ipad") {
+		activeMovie.width = 400;
+		activeMovie.height = 300;
+	}
+	win.add(activeMovie);
+}
+
+// label 
+var movieLabel = Titanium.UI.createLabel({
+	text:'Do not try this at home',
+	width:'auto',
+	height:35,
+	color:'white',
+	font:{fontSize:24,fontFamily:'Helvetica Neue'}
+});
+
+// add label to view
+activeMovie.add(movieLabel);
+
+// label click
+movieLabel.addEventListener('click',function()
+{
+	movieLabel.text = "You clicked the video label. Sweet!";
+	if (android) {
+		activeMovie.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_NONE;
+	}
+});
+
+activeMovie.addEventListener('load',function()
+{
+	// animate label
+	var t = Titanium.UI.create2DMatrix();
+	t = t.scale(3);
+	movieLabel.animate({transform:t, duration:500, color:'red'},function()
+	{
+		var t = Titanium.UI.create2DMatrix();
+		movieLabel.animate({transform:t, duration:500, color:'white'});
+	});
+});
+activeMovie.addEventListener('complete',function()
+{
+	var dlg = Titanium.UI.createAlertDialog({title:'Movie', message:'Completed!'});
+	if (Ti.Platform.name == 'android') {
+		dlg.addEventListener('click', function(e) {
+			activeMovie.hide();
+			win.close();
+		});
+		dlg.show();
+	} else {
+		dlg.show();
+		win.close();
+	}
+});
+
+activeMovie.addEventListener('playbackState',function(e){
+    Ti.API.info('Event PlaybackState Fired: '+e.playbackState);
+    Ti.API.info('activeMovie.endPlaybackTime: '+activeMovie.endPlaybackTime);
+    Ti.API.info('activeMovie.playableDuration: '+activeMovie.playableDuration);
+});
+
+activeMovie.play();
+
+win.addEventListener('close', function() {
+	activeMovie.stop();
+});
