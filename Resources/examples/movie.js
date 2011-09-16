21a1865eb9caf7ac885bf8b07be7dc9a0ef81858
tonylukasavage <anthony.lukasavage@gmail.com>
2011-09-16 11:00:43 -0400

    fixed misc -> movie navigation and movie alert dialog

--- Resources/examples/movie.js
+++ Resources/examples/movie.js
@@ -3,22 +3,6 @@ var data = [
 	{title:'Local', hasChild:true, test:'../examples/movie_local.js'}
 ];
 
-Ti.include("version.js");
-
-
-if (isIPhone3_2_Plus())
-{
-	// can only test this support on a 3.2+ device
-	data.push({title:'Embedded Video', hasChild:true, test:'../examples/movie_embed.js'});
-}
-
-// add iphone specific tests
-if (Titanium.Platform.name == 'iPhone OS')
-{
-	data.push({title:'Remote Streaming', hasChild:true, test:'../examples/movie_remote.js'});
-	data.push({title:'Remote Streaming 2', hasChild:true, test:'../examples/movie_remote2.js'});
-}
-
 // create table view
 var tableview = Titanium.UI.createTableView({
 	data:data
--- Resources/examples/movie_local.js
+++ Resources/examples/movie_local.js
@@ -1,37 +1,19 @@
 var win = Titanium.UI.currentWindow;
-var android = (Titanium.Platform.name == 'android');
 
 var options = {
 	contentURL:'../movie.mp4',
 	backgroundColor:'#111',
-	scalingMode:Titanium.Media.VIDEO_SCALING_MODE_FILL
+	scalingMode:Titanium.Media.VIDEO_SCALING_MODE_FILL,
+	mediaControlStyle: Titanium.Media.VIDEO_CONTROL_DEFAULT
 };
-
-if (android) {
-	options.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_DEFAULT;
-} else {
-	if (parseFloat(Titanium.Platform.version) >= 3.2) {
-		options.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_NONE;
-	} else {
-		options.movieControlMode = Titanium.Media.VIDEO_CONTROL_NONE;
-	}
-}
-
 var activeMovie = Titanium.Media.createVideoPlayer(options);
 
 if (parseFloat(Titanium.Platform.version) >= 3.2)
 {
 	activeMovie.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_EMBEDDED;
-//	activeMovie.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_FULLSCREEN;
-//	activeMovie.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_NONE;
-	if (Titanium.Platform.osname == "ipad") {
-		activeMovie.width = 400;
-		activeMovie.height = 300;
-	}
 	win.add(activeMovie);
 }
 
-// label 
 var movieLabel = Titanium.UI.createLabel({
 	text:'Do not try this at home',
 	width:'auto',
@@ -39,17 +21,13 @@ var movieLabel = Titanium.UI.createLabel({
 	color:'white',
 	font:{fontSize:24,fontFamily:'Helvetica Neue'}
 });
-
-// add label to view
 activeMovie.add(movieLabel);
 
 // label click
 movieLabel.addEventListener('click',function()
 {
 	movieLabel.text = "You clicked the video label. Sweet!";
-	if (android) {
-		activeMovie.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_NONE;
-	}
+	activeMovie.mediaControlStyle = Titanium.Media.VIDEO_CONTROL_NONE;
 });
 
 activeMovie.addEventListener('load',function()
@@ -65,17 +43,12 @@ activeMovie.addEventListener('load',function()
 });
 activeMovie.addEventListener('complete',function()
 {
-	var dlg = Titanium.UI.createAlertDialog({title:'Movie', message:'Completed!'});
-	if (Ti.Platform.name == 'android') {
-		dlg.addEventListener('click', function(e) {
-			activeMovie.hide();
-			win.close();
-		});
-		dlg.show();
-	} else {
-		dlg.show();
+	var dlg = Titanium.UI.createAlertDialog({title:'Movie', message:'Completed!', buttonNames:['OK']});
+	dlg.addEventListener('click', function(e) {
+		activeMovie.hide();
 		win.close();
-	}
+	});
+	dlg.show();
 });
 
 activeMovie.addEventListener('playbackState',function(e){
